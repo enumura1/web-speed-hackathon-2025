901c750d31e3660124aff64da54524f7d4bccd58
enumura1 <akirayuto0718@gmail.com>
2025-03-23 12:35:41 +0900

    defer

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -52,7 +52,7 @@ export function registerSsr(app: FastifyInstance): void {
         <head>
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <script src="/public/main.js"></script>
+          <script src="/public/main.js" defer></script>
         </head>
         <body></body>
       </html>
